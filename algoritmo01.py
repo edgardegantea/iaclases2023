2675ad77275a3a2368f84c87a749322575ec3bfa
edgardegantea <edgar.degante.a@gmail.com>
2023-09-09 15:33:25 -0600

    Algoritmo 1. Semáforo

---
+++ algoritmo01.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+print("Algoritmo de funcionamiento de Semáforo")
+
+delayGreen = 5
+delayYellow = 3
+delayRed = 5
+
+while True:
+    print("Verde")
+    sleep(delayGreen)
+    print("Amarillo")
+    sleep(delayYellow)
+    print("Rojo")
+    sleep(delayRed)
+    
+

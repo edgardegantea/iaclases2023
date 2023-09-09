af6f54f828110d7f76a9a3361c6f4e543d3c9c95
edgardegantea <edgar.degante.a@gmail.com>
2023-09-09 15:38:53 -0600

    Algoritmo de semáforo V1

--- algoritmo01.py
+++ algoritmo01.py
@@ -13,5 +13,21 @@ while True:
     sleep(delayYellow)
     print("Rojo")
     sleep(delayRed)
-    
+
+
+
+'''
+Algoritmo
+
+1. Inicio
+2. Definición de variables y constantes
+3. Ciclo Mientras = True
+    a. Encender Color Verde
+    b. delay de color verde (delayGreen)
+    c. Encender Color Amarillo
+    d. delay de color amarillo (delayYellow)
+    e. Encender Color Rojo
+    f. delay de color rojo (delayRed)
+4. Fin
+'''
 
